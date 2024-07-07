26d6482e0773d5ba3a70a8fa491faeacbb36db9f
ProfAndersonAndrade <andersonandrades@prof.educacao.sp.gov.br>
2024-07-06 22:34:11 -0300

    st2.1

--- stream3.py
+++ stream3.py
@@ -1,30 +1,43 @@
 import streamlit as st
 from PIL import Image, ImageFont, ImageDraw
+import io
+import os
 
 def text_on_image(image, text, font_size, color):
     try:
         img = Image.open(image)
-        font = ImageFont.truetype('arial.ttf', font_size)
+        try:
+            font = ImageFont.truetype('Ubuntu-B.ttf', font_size)
+        except IOError:
+            st.warning("Fonte arial.ttf não encontrada. Usando fonte padrão.")
+            font = ImageFont.load_default()
+        
         draw = ImageDraw.Draw(img)
 
         iw, ih = img.size
-         # Calculate the size of the text
+        # Calculate the size of the text
         bbox = draw.textbbox((0, 0), text, font=font)
-        fw, fh = bbox[2] - bbox[0], bbox[3] - bbox[1]
 
-        draw.text(
-            (iw - fw)/2, (ih-fh)/2,
-            text,
-            font=font,
-            fill=color
-        )
+        # A largura e altura do texto são calculadas a partir das coordenadas da caixa delimitadora
+        fw = bbox[2] - bbox[0]
+        fh = bbox[3] - bbox[1]
 
-        img.save('D:/OneDrive/Imagens/Meme para chat/last_image.jpeg')
-        return 'last_image.jpeg'
+        # Calcule a posição para centralizar o texto
+        x = (iw - fw) / 2
+        y = (ih - fh) / 2
+
+        # Desenhe o texto na imagem
+        draw.text((x, y), text, fill=color, font=font)
+
+        # Salve a imagem resultante em um objeto BytesIO para evitar problemas de armazenamento
+        output_image = io.BytesIO()
+        img.save(output_image, format='JPEG')
+        output_image.seek(0)
+
+        return output_image
     except Exception as e:
         st.error(f"An error occurred: {e}")
         return None
-
 
 image = st.file_uploader('Uma imagem', type=['jpg','jpeg'])
 text = st.text_input('Sua marca dágua')
@@ -34,14 +47,16 @@ color = st.selectbox(
 font_size = st.number_input('Tamanho da fonte', min_value=50)
 
 if image:
-    result = st.button(
-        'Aplicar',
-        type='primary',
-        on_click= text_on_image,
-        args=(image, text, font_size, color)
-    )
-    if result:
-        st.image('last_image.jpeg')
+    if st.button('Aplicar',type='primary'):
+        result = text_on_image(image, text, font_size, color)
+        if result:
+            st.image(result)            
+            st.download_button(
+                'Baixe agora mesmo sua foto modificada',
+                file_name='Imagem_modificada.jpg',
+                data = result,
+                mime='image/jpg'
+                )
 else:
     st.warning('Ainda não temos imagem!')
-    
+
